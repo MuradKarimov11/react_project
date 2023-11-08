75a602bb33e5ae3d8cfd234d505a3e17e5e445c7
Murad <muradkarimov>
2023-11-08 11:51:16 +0400

    modified search panel

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -15,7 +15,8 @@ class App extends Component {
                 {name: 'John C.', salary: 800, increase: false, rise: true, id: 1},
                 {name: 'Alex M.', salary: 3000, increase: true, rise: false, id: 2},
                 {name: 'Carl W.', salary: 5000, increase: false, rise: false, id: 3},
-            ]
+            ],
+            term: ''
         }
         this.maxId = 4;
     }
@@ -55,22 +56,37 @@ class App extends Component {
         }))
     }
 
-    render() {
+    searchEmp = (items, term) => {
+        if(term.length === 0) {
+            return items;
+        }
 
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1
+        });
+    }
+
+    onUpdataSearch = (term) => {
+        this.setState({term});
+    }
+
+    render() {
+        const {data, term} = this.state;
         const employers = this.state.data.length;
         const increased = this.state.data.filter(item => item.increase).length;
+        const visibleData = this.searchEmp(data, term);
 
         return (
             <div className="app">
                 <AppInfo employers={employers} increased={increased}/>
     
                 <div className="search-panel">
-                    <SearchPanel/>
+                    <SearchPanel onUpdataSearch={this.onUpdataSearch}/>
                     <AppFilter/>
                 </div>
     
                 <EmployersList 
-                    data={this.state.data}
+                    data={visibleData}
                     onDelete={this.deleteItem}
                     onToggleProp={this.onToggleProp}/>
                 <EmployersAddForm onAdd={this.addItem}/>
